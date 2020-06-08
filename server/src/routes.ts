873700f6cb0222012e3bdbebbec85d813b88e2c3
Amauri <amauri.tavares@ceuma.br>
2020-06-08 18:02:26 -0300

    adicionando rotas de points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,22 +1,57 @@
-import express, { response } from 'express';
+import express, { response, request } from 'express';
 import knex from './database/connections';
 
-const routes = express.Router();
+const routes = express.Router(); 
 
 routes.get('/', (request, response) =>{
     return response.json({message: 'Hello World'});
  });
 
 routes.get('/items', async(request, response) => {
-    
     const items = await knex('items').select('*');
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             title: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}`,
         };
     });
     return response.json(serializedItems);
+});
+
+routes.post('/points', async(request, response) =>{
+    //recurso de desestruturação do Javascript
+    const {
+        name,
+        email,
+        whatsap,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+   const ids = await knex('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsap,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+
+    const pointItems = items.map((item_id: number) => {
+        return{
+            item_id,
+            point_id: ids[0],
+        };
+    })
+    await knex('point_items').insert(pointItems);
+
+    return response.json({success: true});
 });
 
  export default routes;
