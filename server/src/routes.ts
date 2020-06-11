9f60b6552d7fabb17dc95459469eb2a284a936ab
Amauri <amauri.tavares@ceuma.br>
2020-06-11 07:06:39 -0300

    isolando regras nos controllers e criando transactions

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,61 +1,22 @@
 import express, { response, request } from 'express';
-import knex from './database/connections';
+import PointsController from './controllers/PointsControlle';
+import ItemsController from './controllers/ItemsControlle';
 
 const routes = express.Router(); 
 
-routes.get('/', (request, response) =>{
-    return response.json({message: 'Hello World'});
- });
+const pointsController = new PointsController();
+const itemsControlle = new ItemsController();
 
- routes.get('/points', async(request, response)=>{
-     const points = await knex('points').select('*');
-     return response.json(points);
- });
 
-routes.get('/items', async(request, response) => {
-    const items = await knex('items').select('*');
-    const serializedItems = items.map(item => {
-        return {
-            id: item.id,
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`,
-        };
-    });
-    return response.json(serializedItems);
-});
 
-routes.post('/points', async(request, response) =>{
-    //recurso de desestruturação do Javascript
-    const {
-        name,
-        email,
-        whatsap,
-        latitude,
-        longitude,
-        city,
-        uf,
-        items
-    } = request.body;
+routes.get('/items', itemsControlle.index);
+routes.get('/points', pointsController.index);
+routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
 
-   const ids = await knex('points').insert({
-        image: 'image-fake',
-        name,
-        email,
-        whatsap,
-        latitude,
-        longitude,
-        city,
-        uf
-    });
+// index, show, create, update, delete
 
-    const pointItems = items.map((item_id: number) => {
-        return{
-            item_id,
-            point_id: ids[0],
-        };
-    })
-    await knex('point_items').insert(pointItems);
-    return response.json({success: true});
-});
+// Service Patter
+// Repository Patter (Data Mapper)
 
  export default routes;
