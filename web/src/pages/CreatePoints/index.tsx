88ad2ab88688f8848ec412cc31bac50a7a898889
Amauri <amauri.tavares@ceuma.br>
2020-06-19 19:51:11 -0300

    adicionando map na aplicação

--- web/src/pages/CreatePoints/index.tsx
+++ web/src/pages/CreatePoints/index.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import './styles.css';
 import logo from '../../assets/logo.svg';
 import { FiArrowLeft, FiLogIn } from 'react-icons/fi';
+import {Map, TileLayer, Marker }  from 'react-leaflet';
 
 const CreatePoints = () => {
     return (
@@ -21,17 +22,100 @@ const CreatePoints = () => {
                     <legend>
                         <h2>Dados</h2>
                     </legend>
+                    <div className="field">
+                        <label htmlFor="name">Nome da Entidade</label>
+                        <input 
+                        type="text"
+                        name="name"
+                        id="name"
+                        />
+                    </div>
+
+                    <div className="field-group">
+                    <div className="field">
+                        <label htmlFor="email">E-mail</label>
+                        <input 
+                        type="email"
+                        name="email"
+                        id="email"
+                        />
+                    </div>
+                    <div className="field">
+                        <label htmlFor="whatsap">Whatsap</label>
+                        <input 
+                        type="text"
+                        name="whatsap"
+                        id="whatsap"
+                        />
+                    </div>
+                    </div>
                 </fieldset>
+
                 <fieldset>
                     <legend>
                         <h2>Endereço</h2>
+                        <span>Selecione o endereço no mapa</span>
                     </legend>
+
+                   <Map center={[-2.5601377, -44.2384228]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker position={[-2.5601377, -44.2384228]} />
+                   </Map>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">Estado (UF)</label>
+                            <select name="uf" id="uf">
+                                <option value="0">Selecione um UF</option>
+                            </select>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select name="city" id="city">
+                                <option value="0">Selecione uma Cidade</option>
+                            </select>
+                        </div>
+                    </div>
                 </fieldset>
+
                 <fieldset>
                     <legend>
                         <h2>Ítens de Coleta</h2>
+                        <span>Selecione um ou mais itens abaixo</span>
                     </legend>
+                    <ul className="items-grid">
+                        <li className="selected">
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="oleo"/>
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="oleo"/>
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="oleo"/>
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="oleo"/>
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="oleo"/>
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="oleo"/>
+                            <span>Óleo de Cozinha</span>
+                        </li>
+                    </ul>
                 </fieldset>
+
+                <button type="submit">Cadastrar ponto de coleta</button>
                </form>
        </div>
     );
